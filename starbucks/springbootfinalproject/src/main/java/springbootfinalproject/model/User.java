4d27296c51f47c02319067b47910b508d7884cab
Poorva <poorvaa24@gmail.com>
2019-05-04 15:29:41 -0700

    Adding Login API

---
+++ starbucks/springbootfinalproject/src/main/java/springbootfinalproject/model/User.java
@@ -0,0 +1,91 @@
+package springbootfinalproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name="user")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+	
+	@Id
+	//@GeneratedValue(strategy=GenerationType.AUTO)
+	private String username;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@NotBlank
+	private String fname;
+	
+	@NotBlank
+	private String lname;
+	
+	
+	@NotBlank
+	private String password;
+	
+	
+	private int phone;
+	
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	@NotBlank
+	private String city;
+	
+	
+
+}

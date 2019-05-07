ab2ad5c51467ba0c38838b1e3aec1dbf0b274e27
Poorva <poorvaa24@gmail.com>
2019-05-07 14:04:59 -0700

    Added code for sign up

--- Starbucks-UI/app/src/main/java/edu/sjsu/android/starbucks_ui/RegisterActivity.java
+++ Starbucks-UI/app/src/main/java/edu/sjsu/android/starbucks_ui/RegisterActivity.java
@@ -1,17 +1,134 @@
 package edu.sjsu.android.starbucks_ui;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btn_save;
+    private TextView txt_view;
+    private EditText txt_username, txt_password, txt_fname, txt_lname, txt_phone , txt_city;
+    String username, password, fname, lname, phone, city;
+    String serviceURL = "http://ec2-54-185-174-206.us-west-2.compute.amazonaws.com:5000/users";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txt_username      =   (EditText)findViewById(R.id.enterUserName);
+        txt_password      =   (EditText)findViewById(R.id.enterPassword);
+        txt_fname      =   (EditText)findViewById(R.id.enterFirstName);
+        txt_lname      =    (EditText)findViewById(R.id.enterLastName);
+        txt_phone       =   (EditText)findViewById(R.id.enterPhone);
+        txt_city =    (EditText)findViewById(R.id.enterCity);
+        txt_view = (TextView)findViewById(R.id.response);
+
+        txt_view.setText("Result");
+        btn_save = (Button)findViewById(R.id.signUp);
+
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HTTPAsyncTask runner = new HTTPAsyncTask();
+
+                runner.execute(serviceURL);
+            }
+        });
     }
 
+    private class HTTPAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                try {
+                    return HttpPost(urls[0]);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return "Error!";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            txt_view.setText(result);
+        }
+    }
+
+    private String HttpPost(String myUrl) throws IOException, JSONException {
+        String result = "";
+
+        URL url = new URL(myUrl);
+
+        // 1. create HttpURLConnection
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+        // 2. build JSON object
+        JSONObject jsonObject = buidJsonObject();
+
+        // 3. add JSON content to POST request body
+        setPostRequestContent(conn, jsonObject);
+
+        // 4. make POST request to the given URL
+        conn.connect();
+
+        // 5. return response messag
+        String responseMsg = conn.getResponseMessage();
+        int code = conn.getResponseCode();
+        responseMsg += "";
+
+        Log.i(MainActivity.class.toString(), responseMsg + "    " + code);
+
+        return responseMsg;
+    }
+
+    private JSONObject buidJsonObject() throws JSONException {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate("username", txt_username.getText().toString());
+        jsonObject.accumulate("password",  txt_password.getText().toString());
+        jsonObject.accumulate("fname",  txt_fname.getText().toString());
+        jsonObject.accumulate("lname",  txt_lname.getText().toString());
+        jsonObject.accumulate("phone",  txt_phone.getText().toString());
+        jsonObject.accumulate("city",  txt_city.getText().toString());
+        return jsonObject;
+    }
+
+    private void setPostRequestContent(HttpURLConnection conn,
+                                       JSONObject jsonObject) throws IOException {
+
+        OutputStream os = conn.getOutputStream();
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+        writer.write(jsonObject.toString());
+        Log.i(MainActivity.class.toString(), jsonObject.toString());
+        writer.flush();
+        writer.close();
+        os.close();
+    }
     public void closeApp(View v){
         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
         startActivity(intent);

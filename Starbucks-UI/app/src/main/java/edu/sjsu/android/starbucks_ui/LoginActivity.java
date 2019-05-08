b99e667a57bb344260c93c27656f02abfc160ab5
Poorva <poorvaa24@gmail.com>
2019-05-07 21:06:13 -0700

    API changes login

--- Starbucks-UI/app/src/main/java/edu/sjsu/android/starbucks_ui/LoginActivity.java
+++ Starbucks-UI/app/src/main/java/edu/sjsu/android/starbucks_ui/LoginActivity.java
@@ -1,16 +1,127 @@
 package edu.sjsu.android.starbucks_ui;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private Button btn_login;
+    private EditText txt_username;
+    private EditText txt_password;
+    String serviceURL = "http://ec2-54-185-174-206.us-west-2.compute.amazonaws.com:5000/validUsers?username="+txt_username+"&password="+txt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = (Button)findViewById(R.id.login);
+        txt_username = (EditText)findViewById(R.id.enterUserName);
+        txt_password = (EditText)findViewById(R.id.enterPassword);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                LoginActivity.HttpGetRequest runner = new LoginActivity.HttpGetRequest();
+
+                runner.execute(serviceURL);
+            }
+        });
+    }
+
+    public class HttpGetRequest extends AsyncTask<String, Void, String> {
+        public static final String REQUEST_METHOD = "GET";
+        public static final int READ_TIMEOUT = 15000;
+        public static final int CONNECTION_TIMEOUT = 15000;
+
+        @Override
+        protected String doInBackground(String... params) {
+            String stringUrl = params[0];
+
+            try {
+                try {
+                    return HttpGET(params[0]);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return "Error!";
+                }
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        private String HttpGET(String Url) throws IOException, JSONException {
+            String apiResponse;
+            String inputLine;
+
+                //Create a URL object holding our url
+                URL myUrl = new URL(Url);
+
+                //Create a connection
+                HttpURLConnection connection =(HttpURLConnection)
+                        myUrl.openConnection();
+
+                //Set methods and timeouts
+                connection.setRequestMethod(REQUEST_METHOD);
+                connection.setReadTimeout(READ_TIMEOUT);
+                connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                //Connect to our url
+                connection.connect();
+
+                //Create a new InputStreamReader
+                InputStreamReader streamReader = new
+                        InputStreamReader(connection.getInputStream());
+
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                //Check if the line we are reading is not null
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine);
+                }
+
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+
+                //Set our result equal to our stringBuilder
+                apiResponse = stringBuilder.toString();
+            JSONObject response = null;
+
+                response = new JSONObject(apiResponse);
+                String message = response.getString("result");
+                Log.i(MainActivity.class.toString(), message + " --------  YAYAYAYAYAY  " );
+                return message;
+                
+        }
+
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+        }
+
     }
 
     public void openDashboard(View v){
--- src/main/java/com/project/mysystemproject/controller/UserController.java
+++ src/main/java/com/project/mysystemproject/controller/UserController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.project.mysystemproject.model.User;
 import com.project.mysystemproject.repository.UserRepository;
 import javax.validation.Valid;
-import java.util.List;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class UserController {
-
+	
 	@Autowired
     UserRepository userRepository;
     
@@ -20,7 +21,6 @@ public class UserController {
 	@ResponseBody
 	public List<User> getUsers(@RequestParam(value = "username", required = false) String userName) 
 	{
-
 		if (userName == null || userName.isEmpty()) {
 			return userRepository.findAll();
 		} else {
@@ -37,26 +37,29 @@ public class UserController {
 		if (listOfUser.size() == 0) // Its a new user 
 		{
 			userRepository.save(user);
-			responseMap.put("result","Successfuly test");
+			responseMap.put("result","Successfully registered");
 			return responseMap;
 			//return true;
 		}
-		responseMap.put("result","Username already exists");
+		responseMap.put("result","User already exists!");
 		return responseMap;
 		//return false;
 	}
 	
 	@GetMapping(path = "/validUsers", produces = "application/json; charset=UTF-8")
 	@ResponseBody
-	public boolean isValidUser(@RequestParam(value = "username") String username,@RequestParam(value = "password") String password)
+	public Map<String, String> isValidUser(@RequestParam(value = "username") String username,@RequestParam(value = "password") String password)
 	{
+		Map<String, String> responseMap = new HashMap<String,String>();
 		List<User> listOfUser = userRepository.findByusernameAndpassword(username,password);
 		if(listOfUser.size() > 0)
 		{
-			return true;
+			responseMap.put("result","Successfully Logged In");
+			return responseMap;
 		}
 		
-		return false;
+		responseMap.put("result","Invalid credentials");
+		return responseMap;
 		
 	}
     
